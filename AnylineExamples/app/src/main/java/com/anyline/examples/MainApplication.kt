edf5f8f47e6242c8bb22c99f64f46efd93f746e0
Patrick Fekete <patrick@anyline.com>
2023-06-01 15:54:22 +0200

    Update SDK version to 47.0.0

--- AnylineExamples/app/src/main/java/com/anyline/examples/MainApplication.kt
+++ AnylineExamples/app/src/main/java/com/anyline/examples/MainApplication.kt
@@ -1,6 +1,7 @@
 package com.anyline.examples
 
 import android.app.Application
+import io.anyline2.AnylineSdk
 import io.anyline2.core.LicenseException
 
 import timber.log.Timber
@@ -16,9 +17,8 @@ class MainApplication : Application() {
         // This must be called before doing anything Anyline-related!
         // Try/Catch this to check whether or not your license key is valid!
         try {
-            io.anyline2.AnylineSdk.init(getString(R.string.anyline_license_key), this)
-        }
-        catch (e: LicenseException) {
+            AnylineSdk.init(getString(R.string.anyline_license_key), this)
+        } catch (e: LicenseException) {
             // handle exception
             Timber.tag("LicenseException").e(e)
         }
