1edcec3599c1d1d86e6c780572d9f3adafc6ce3e
igor-graca <80965280+igor-graca@users.noreply.github.com>
2025-02-13 15:07:16 +0100

    Release 54.6.0 (#94)

--- AnylineExamples/app/src/main/java/com/anyline/examples/MainApplication.kt
+++ AnylineExamples/app/src/main/java/com/anyline/examples/MainApplication.kt
@@ -2,11 +2,15 @@ package com.anyline.examples
 
 import android.app.Application
 import io.anyline2.AnylineSdk
-import io.anyline2.core.LicenseException
+import io.anyline2.init.SdkInitializationConfig
+import io.anyline2.init.SdkInitializationListener
+import io.anyline2.init.SdkInitializationParameters
+import io.anyline2.init.SdkInitializationState
+import io.anyline2.init.SdkInitializationStrategy
 
 import timber.log.Timber
 
-class MainApplication : Application() {
+class MainApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
@@ -14,14 +18,28 @@ class MainApplication : Application() {
             Timber.plant(Timber.DebugTree())
         }
 
-        // This must be called before doing anything Anyline-related!
-        // Try/Catch this to check whether or not your license key is valid!
-        try {
-            AnylineSdk.init(getString(R.string.anyline_license_key),
-                this)
-        } catch (e: LicenseException) {
-            // handle exception
-            Timber.tag("LicenseException").e(e)
-        }
+        AnylineSdk.init(this, SdkInitializationConfig(
+            sdkInitializationParameters = SdkInitializationParameters(
+                licenseKey = getString(R.string.anyline_license_key)),
+            sdkInitializationStrategy = SdkInitializationStrategy.AsyncAuto(object: SdkInitializationListener {
+                override fun onInitializationStarted() {
+                    Timber
+                        .tag("AnylineSdkInit")
+                        .d("Anyline SDK initialization in progress...")
+                }
+
+                override fun onInitializationFailed(state: SdkInitializationState.NotInitialized) {
+                    Timber
+                        .tag("AnylineSdkInit")
+                        .e("Anyline SDK not initialized: ${state.lastError?.exception?.message}")
+                }
+
+                override fun onInitializationSucceeded(state: SdkInitializationState.Initialized) {
+                    Timber
+                        .tag("AnylineSdkInit")
+                        .d("Anyline SDK initialized!")
+                }
+            }))
+        )
     }
 }
